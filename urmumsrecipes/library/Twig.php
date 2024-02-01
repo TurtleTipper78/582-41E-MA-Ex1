232b38a55b886e93047800f22caa28f5838afd83
picklerules <136508588+picklerules@users.noreply.github.com>
2024-02-01 17:27:09 -0500

    architecture MVC

---
+++ urmumsrecipes/library/Twig.php
@@ -0,0 +1,19 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class Twig{
+    static public function render($template, $data = array()){
+        $loader = new \Twig\Loader\FilesystemLoader('view');
+        $twig = new  \Twig\Environment($loader, array('auto_reload' => true));
+        
+
+        
+        echo $twig->render($template, $data);
+    }
+
+}
+
+?>
